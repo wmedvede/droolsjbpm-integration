88dcaa6c964f447c082fb572de667eee085ca3ff
Walter Medvedeo <wmedvedeo@gmail.com>
2019-11-04 08:50:18 +0100

    PLANNER-1623: [Tasks Assignment] Planning API iteration #1     - IV (PLANNER-1623 and PLANNER-1625)

--- jbpm-task-assigning/jbpm-task-assigning-runtime/jbpm-task-assigning-runtime-service/src/main/java/org/jbpm/task/assigning/runtime/service/util/PropertyUtil.java
+++ jbpm-task-assigning/jbpm-task-assigning-runtime/jbpm-task-assigning-runtime-service/src/main/java/org/jbpm/task/assigning/runtime/service/util/PropertyUtil.java
@@ -15,15 +15,17 @@ public class PropertyUtil {
     private PropertyUtil() {
     }
 
-    public static <T, E extends Exception> T readSystemProperty(String propertyName, T defaultValue, PropertyParser<String, T, E> parser) {
+    public static <T, E extends Exception> T readSystemProperty(String propertyName,
+                                                                T defaultValue,
+                                                                PropertyParser<String, T, E> parser) {
         String strValue = null;
         try {
             strValue = System.getProperty(propertyName);
             if (strValue == null) {
-                LOGGER.debug("Property: {}  was not configured. Default value will be used instead: {}", propertyName, defaultValue);
+                LOGGER.debug("Property: {}  was not configured. Default value will be used instead: {}", propertyName,
+                             defaultValue);
                 return defaultValue;
             }
-
             return parser.parse(strValue);
         } catch (Exception e) {
             LOGGER.error("An error was produced while parsing " + propertyName + " value from string: " + strValue +
